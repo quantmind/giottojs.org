6aca5c841b9dcfc08765bb646985e1b78b620b66
Luca Sbardella <luca@quantmind.com>
2016-11-26 11:21:38 +0000

    icons

--- giottojs/config.py
+++ giottojs/config.py
@@ -33,21 +33,38 @@ CONTENT_GROUPS = {
     "context": {}
 }
 
-HTML_LINKS = ['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/'
-              '4.3.0/css/font-awesome.min.css',
-              {'href': 'giottojs/sandstone', 'id': 'giotto-theme'}]
-HTML_SCRIPTS = ['giottojs/giottojs']
+HTML_SCRIPTS = ['giottojs']
 
-HTML_META = [{'http-equiv': 'X-UA-Compatible',
-              'content': 'IE=edge'},
-             {'name': 'viewport',
-              'content': 'width=device-width, initial-scale=1'},
-             {'name': 'description', 'content': DESCRIPTION}]
-
-LINKS = {'AngularJS': 'https://angularjs.org/',
-         'RequireJS': 'http://requirejs.org/',
-         'd3': 'http://d3js.org/',
-         'd3js': 'http://d3js.org/'}
+HTML_LINKS = [
+    "giottojs.css",
+    {"rel": "apple-touch-icon", "sizes": "57x57", "href": "/icons/apple-icon-57x57.png"},
+    {"rel": "apple-touch-icon", "sizes": "57x57", "href": "/icons/apple-icon-57x57.png"},
+    {"rel": "apple-touch-icon", "sizes": "60x60", "href": "/icons/apple-icon-60x60.png"},
+    {"rel": "apple-touch-icon", "sizes": "72x72", "href": "/icons/apple-icon-72x72.png"},
+    {"rel": "apple-touch-icon", "sizes": "76x76", "href": "/icons/apple-icon-76x76.png"},
+    {"rel": "apple-touch-icon", "sizes": "114x114", "href": "/icons/apple-icon-114x114.png"},
+    {"rel": "apple-touch-icon", "sizes": "120x120", "href": "/icons/apple-icon-120x120.png"},
+    {"rel": "apple-touch-icon", "sizes": "144x144", "href": "/icons/apple-icon-144x144.png"},
+    {"rel": "apple-touch-icon", "sizes": "152x152", "href": "/icons/apple-icon-152x152.png"},
+    {"rel": "apple-touch-icon", "sizes": "180x180", "href": "/icons/apple-icon-180x180.png"},
+    {"rel": "apple-touch-icon", "sizes": "152x152", "href": "/icons/apple-icon-152x152.png"},
+    {"rel": "apple-touch-icon", "sizes": "152x152", "href": "/icons/apple-icon-152x152.png"},
+    {"rel": "icon", "type": "image/png", "sizes": "16x16", "href": "/icons/favicon-16x16.png"},
+    {"rel": "icon", "type": "image/png", "sizes": "32x32", "href": "/icons/favicon-32x32.png"},
+    {"rel": "icon", "type": "image/png", "sizes": "96x96", "href": "/icons/favicon-96x96.png"},
+    {"rel": "icon", "type": "image/png", "sizes": "192x192", "href": "/icons/android-icon-192x192.png"},
+    {"rel": "manifest", "href": "/icons/manifest.json"}
+]
+HTML_META = [
+    {'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge'},
+    {'name': 'viewport', 'content': 'width=device-width, initial-scale=1'},
+    {'name': 'msapplication-TileColor', 'content': '#ffffff'},
+    {'name': 'msapplication-TileImage', 'content': '/icons/ms-icon-144x144.png'},
+    {'name': 'theme-color', 'content': '#ffffff'},
+    {'name': 'description', 'content': DESCRIPTION}
+]
 
 OAUTH_PROVIDERS = {'google': {'analytics': {'id': 'UA-54439804-4'}},
                    'twitter': {'site': '@quantmind'}}
+
+workers = 0
